34c74cf15d7f1c222d5a6617d1e4e937bee203c3
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-10-02 23:33:09 +0300

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
+import authRouter from "./routes/api/auth-router.js";
 import contactsRouter from "./routes/api/contacts.js";
 
 import dotenv from "dotenv";
@@ -17,10 +18,11 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found 1" });
 });
 
 app.use((err, req, res, next) => {
--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -5,7 +5,13 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({});
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "", { skip, limit }).populate(
+    "owner",
+    "email"
+  );
   res.json(result);
 };
 
@@ -21,7 +27,8 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({...req.body, owner});
   res.status(201).json(result);
 };
 
