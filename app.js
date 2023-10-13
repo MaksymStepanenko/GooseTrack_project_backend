ec405a5f6de2adc149f2218a0127c8b8521fc1c7
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-10-14 00:13:51 +0300

    add setting

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-import authRouter from "./routes/api/auth-router.js";
-import contactsRouter from "./routes/api/contacts.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 
 
 const app = express();
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
@@ -19,8 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found 1" });
