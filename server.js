1d149916f3497dc5f0b7ce24c90c8dc833409414
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-10-14 14:12:51 +0300

    init error message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ mongoose
   .connect(process.env.DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log("Server running");
     });
   })
   .catch((error) => {
