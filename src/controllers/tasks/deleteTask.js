a808cc014b8904a2cc66a56bb5780d079a2de9a8
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-10-23 12:57:07 +0300

    fix delete tasks/add check error get

--- src/controllers/tasks/deleteTask.js
+++ src/controllers/tasks/deleteTask.js
@@ -1,15 +1,14 @@
 import { ctrlWrapper } from "../../decorators/index.js";
 import Task from "../../models/tasks.js";
+import checkByError from "../../helpers/checkByError.js" 
 
 const deleteTask = async (req, res) => {
-  const result = await Task.findByIdAndDelete(id);
-  if (!result) {
-    throw HttpError(404, `Contact with id=${id} not found`);
-  }
-
-  res.json({
-    message: "Delete success",
-  });
+  const { id } = req.params;
+  const task = await Task.findById(id);
+  checkByError(task.owner._id.toString() !== req.user._id.toString(), 404);
+  const result = await Task.findByIdAndRemove(id);
+  checkByError(!result, 404);
+  res.json({ message: "delete successful", status: 200 });
 };
 
 
